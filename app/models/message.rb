97bb165147777ecb067e0cc0293b7c9b1e0584a6
m_hagiwara <masahiro.hagiwara@acreation.co.jp>
2016-02-20 07:45:15 +0000

    challenge2 add 20160220

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,10 @@
 class Message < ActiveRecord::Base
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
-    # 年齢は必須入力かつ0文字以上
-  validates :age , length: { minimum: 0 } , presence: true
+  # 年齢は数値でかつ、整数だけ、0歳以上120歳以下
+  validates :age, numericality: {
+    only_integer: true , greater_than_or_equal_to: 0, less_than_or_equal_to: 120
+  }
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
 end
